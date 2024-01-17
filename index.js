c599b6edd92ad6dc2934be180cc008b32da9b490
Nico Lozano <homeronicolaslozano@gmail.com>
2024-01-16 19:54:10 -0500

    Done

---
+++ index.js
@@ -0,0 +1,39 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+  
+  
+  
+  
+  
+  
+  function whisper(string) { 
+    return string.toLowerCase(); 
+  }
+  
+  
+  function logShout(string) {
+    console.log(string.toUpperCase());
+  }
+  function logWhisper(string) {
+    console.log(string.toLowerCase());
+  }
+  
+  function sayHiToHeadphonedRoommate(string) { 
+    if(string === string.toUpperCase()) {
+      return "YES INDEED!" 
+    }
+    else if(string === string.toLowerCase()) {
+      return "I can't hear you!" 
+    } if(string === "Let's have dinner together!") { 
+        return "I would love to!" 
+  }
+  }
+  /*
+  if(string === string.toLowerCase()) {
+    return "I can't hear you!" 
+  }
+  if(string === string.toUpperCase()) {
+    return "Yes Indeed!"
+  } 
+  */
